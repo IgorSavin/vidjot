5d5a1d9445503750745676d254fe55683caca60f
Igor Savin <beixiaofeng@gmail.com>
2018-09-25 23:11:10 +0300

    linked to mongoose

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require ('express');
 const exphbs = require ('express-handlebars');
+const mongoose = require ('mongoose');
 
 const app = express();
+
+//Map global promise - get rid of warning
+// mongoose.Promise = global.Promise;
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev',{
+  // useMongoClient: true
+  useNewUrlParser: true
+})
+.then(()=>console.log('MongoDB connected...'))
+.catch(err => console.log('error'));
+
+//Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 //Handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 
 
 
@@ -21,6 +38,11 @@ res.render('index',{
 
 app.get('/about',(req,res)=>{
   res.render('about');
+});
+
+//Add Idea Form
+app.get('/ideas/add',(req,res)=>{
+  res.render('ideas/add');
 });
 
 const port = 5000;
